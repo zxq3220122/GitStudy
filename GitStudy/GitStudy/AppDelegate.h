d5f0e5f0a7990809cfade66913ae7cf5e4105f0c
zxq3220122 <540765757@qq.com>
2018-10-29 14:35:29 +0800

    新建工程

---
+++ GitStudy/GitStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitStudy
+//
+//  Created by 周洁 on 2018/10/29.
+//  Copyright © 2018年 周洁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitStudy/GitStudy/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  GitStudy
+//
+//  Created by 周洁 on 2018/10/29.
+//  Copyright © 2018年 周洁. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
